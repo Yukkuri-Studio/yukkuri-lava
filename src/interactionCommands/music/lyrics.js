a6074ff9a2980e54ef58bdc0dc72226c5e190bd4
Kiara Amelia <lintanggaming24@gmail.com>
2022-08-03 11:33:01 +0000

    fix lyrics

--- src/interactionCommands/music/lyrics.js
+++ src/interactionCommands/music/lyrics.js
@@ -9,66 +9,66 @@ class Lyrics extends Command {
   constructor(client) {
     super(client, {
       component: new SlashCommandBuilder()
-      .setName("lyrics")
-      .setDescription("Find music lyrics")
-      .addStringOption((opt) => opt
-      .setName("song")
-      .setDescription("Insert the song name"))
+        .setName("lyrics")
+        .setDescription("Find music lyrics")
+        .addStringOption((opt) => opt
+          .setName("song")
+          .setDescription("Insert the song name"))
     })
   }
-  
+
   async run(i) {
     const song = i.options.getString("song")
 
-      if (!song) {
+    if (!song) {
       const player = this.client.music.poru.players.get(i.guild.id)
-      
-      if (!player) return i.reply("No song playing right now")
-      
-      if (!player.currentTrack) return i.reply("No song playing right now")
-      
-    return this.lyricsSender(encodeURI(player.currentTrack.info.title), i)
+
+      if (!player) return await i.reply("No song playing right now")
+
+      if (!player.currentTrack) return await i.reply("No song playing right now")
+
+      return this.lyricsSender(encodeURI(player.currentTrack.info.title), i)
     }
-    
-    return thislyricsSender(song, i)
+
+    return this.lyricsSender(song, i)
   }
-  
+
   async lyricsSender(song, i) {
     try {
       const response = await fetch(`https://api.lxndr.dev/lyrics?song=${song}&from=DiscordRawon`, {
-    method: "get"
-  })
+        method: "get"
+      })
 
-    const res = await response.json()
+      const res = await response.json()
 
-    const lyrics = this.chunk(res.lyrics, 2048)
-    
-    const embed = new EmbedBuilder()
-    .setColor("Red")
-    .setTitle(`${res.song} - ${res.artist}`)
-    .setURL(res.url)
-    .setThumbnail(res.album_art)
-    
-    if (res.lyrics.length > 2048) {
-      i.reply({ content: "_ _" })
-      lyrics.forEach((x) => {
-        embed.setDescription(x)
-        i.channel.send({ embeds: [embed] })
-      });
-      return
-    }
-    
-    embed.setDescription(res.lyrics)
-    i.reply({ embeds: [embed] })
+      const lyrics = this.chunk(res.lyrics, 2048)
+
+      const embed = new EmbedBuilder()
+        .setColor("Red")
+        .setTitle(`${res.song} - ${res.artist}`)
+        .setURL(res.url)
+        .setThumbnail(res.album_art)
+
+      if (res.lyrics.length > 2048) {
+        await i.reply({ content: "_ _" })
+        lyrics.forEach(async (x) => {
+          embed.setDescription(x)
+          await i.reply({ embeds: [embed] })
+        });
+        return
+      }
+
+      embed.setDescription(res.lyrics)
+      await i.reply({ embeds: [embed] })
     } catch (er) {
-      i.reply("No lyrics found.")
+      await i.reply("No lyrics found.")
     }
   }
-  
-  chunk (array, chunkSize){
+
+  chunk(array, chunkSize) {
     const temp = [];
-    for(let i = 0; i < array.length; i+= chunkSize){
-      temp.push(array.slice(i, i+chunkSize));
+    for (let i = 0; i < array.length; i += chunkSize) {
+      temp.push(array.slice(i, i + chunkSize));
     }
     return temp;
   }
