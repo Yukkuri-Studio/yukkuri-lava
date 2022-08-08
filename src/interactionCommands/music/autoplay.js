5d52e26209d4b7d11be1a07d758d355becf0711e
Kiara Amelia <lintanggaming24@gmail.com>
2022-08-08 07:08:08 +0000

    Update 3 files

---
+++ src/interactionCommands/music/autoplay.js
@@ -0,0 +1,33 @@
+const Command = require("../../structures/command");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+class AutoPlay extends Command {
+  constructor(client) {
+    super(client, {
+      component: new SlashCommandBuilder()
+      .setName("autoplay")
+      .setDescription("Autoplaying music"),
+      inVoice: true
+    })
+  }
+  
+  async run(i) {
+    const memberVoice = i.member.voice.channelId;
+
+    if (this.inVoice && !memberVoice) {
+      i.editReply({
+        content: "You must be in voice channel before running this command.",
+      });
+    }
+
+    const player = this.client.music.poru.players.get(i.guild.id);
+
+    if (!player) return i.reply("There is no music playing right now.");
+    
+    await player.setAutoplay(!player.isAutoplay, player.currentTrack)
+    
+    i.reply("Enabled")
+  }
+}
+
+module.exports = AutoPlay
