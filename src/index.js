63f22cf1b51d9c897e2f16cc69920316487f24c9
Kiara Amelia <41572020+ameliakiara@users.noreply.github.com>
2022-08-12 21:13:32 +0700

    Dev (#53)
    * chnage poru version
    
    * add new info command
    
    * fix typo
    
    * Update 3 files
    
    * Update play.js
    
    * Update autoplay.js
    
    * Update 2 files
    
    * Update filter.js and invite.js
    
    * Update loader.js
    
    * Update loader.js
    
    * Update 7 files
    
    * fix: typo
    
    * Update 3 files
    
    * fix: auto leave when there is no one in voice channel
    
    * fix: interval
    
    * fix: auto leave interval
    
    * adding search command
    
    * Dev (#27) (#28)
    
    * chnage poru version
    
    * add new info command
    
    * fix typo
    
    * Update 3 files
    
    * Update play.js
    
    * Update autoplay.js
    
    * Update 2 files
    
    * Update filter.js and invite.js
    
    * Update loader.js
    
    * Update loader.js
    
    * Update 7 files
    
    * fix: typo
    
    * Update 3 files
    
    * fix: auto leave when there is no one in voice channel
    
    * fix: interval
    
    * fix: auto leave interval
    
    * adding search command
    
    Co-authored-by: Gamflaz <ivbalgaming@gmail.com>
    
    Co-authored-by: Gamflaz <ivbalgaming@gmail.com>
    
    * added ping command
    
    * adding bgm command
    
    * change playlist link
    
    * adding new info
    
    * deleting console log
    
    * adding activity
    
    * adding category and change help layout
    
    * adding button
    
    * fix volume
    
    * change djs version
    
    * update package
    
    * added new vote function, 24/7, database, and playlist command
    
    * fix poster
    
    * added premium features
    
    * fix 24/7
    
    * fix reply
    
    * fix date.now
    
    * fix typo
    
    * update premium system using bullmq
    
    * fix volume
    
    * fix bull
    
    * fix
    
    * fix
    
    * fix bull
    
    * fix
    
    * bullmq fix
    
    Co-authored-by: Gamflaz <ivbalgaming@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const client = new Client();
 client.init();
 
 setTimeout(() => {
-	client.timer.checker();
-	client.timer.worker();
+  client.timer.checker();
+  console.log("BullMQ Checker activate.")
 }, 3e3);
+
+setTimeout(() => {
+  client.timer.checker();
+  console.log("BullMQ Worker activate.")
+}, 4e3);
